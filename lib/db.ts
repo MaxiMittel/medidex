27b6126094e5bd39bcd55c4611e4fb3230865005
Maximilian Mittelhammer <maximittel@outlook.de>
2025-10-25 10:05:56 +0200

    hotfix: switch to prisma client

--- lib/db.ts
+++ lib/db.ts
@@ -1,11 +1,14 @@
 import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
 
 /**
  * PrismaClient singleton instance
  * This pattern prevents multiple instances of PrismaClient during development hot reloads
  */
 const prismaClientSingleton = () => {
+  const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL! });
   return new PrismaClient({
+    adapter,
     log:
       process.env.NODE_ENV === "development"
         ? ["query", "error", "warn"]
