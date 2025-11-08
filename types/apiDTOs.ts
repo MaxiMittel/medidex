b4e6407f57d68a767595a13264655352c9445472
Skander Amara <skander.amara@tum.de>
2025-11-08 01:53:22 +0100

    create api client and functions for endpoints

---
+++ types/apiDTOs.ts
@@ -0,0 +1,144 @@
+export interface TokenDto {
+  access_token: string;
+  token_type: string;
+}
+
+export interface BatchDto {
+  batch_hash: string;
+  batch_description: string;
+  number_reports: number;
+  created_at: string;
+  embedded: number;
+  assigned: number;
+}
+
+export interface VectorDto {
+  model_id: string;
+  embedding: number[];
+  author_embedding: number[];
+  participants: number[];
+  intervention: number[];
+  condition: number[];
+  outcome: number[];
+}
+
+export interface ReportDetailDto {
+  title: string;
+  abstract: string | null;
+  authors: string[];
+  trial_id: string | null;
+  vectors: VectorDto;
+  assigned_studies: number[];
+}
+
+export interface SimilarTagDto {
+  id: string;
+  keywords: string;
+  distance: number;
+}
+
+export interface GetSimilarTagsParams {
+  sources: string[];  
+  aspect?: string;      
+  k?: number;           
+}
+
+export interface SimilarStudiesResponseDto {
+  CRGStudyID: number[];
+  Relevance: number[];
+  ShortName: string[];
+  NumberParticipants: string[];
+  Duration: (string | null)[];
+  Comparison: (string | null)[];
+  Countries: string[];
+  DateEntered: string[];
+  DateEdited: string[];
+  StatusofStudy: string[];
+}
+
+export interface GetSimilarStudiesParams {
+  aspect?: string;
+  cutoff?: string;
+  k?: number;
+  return_details?: boolean;
+}
+
+export interface StudyDto {
+  StatusofStudy: string | null;
+  NumberParticipants: string | null;
+  TrialistContactDetails: string | null;
+  Countries: string | null;
+  CENTRALSubmissionStatus: string | null;
+  Duration: string | null;
+  Notes: string | null;
+  UDef4: string | null;
+  CRGStudyID: number;
+  DateEntered: string | null;
+  Comparison: string | null;
+  CENTRALStudyID: number;
+  DateToCENTRAL: string | null;
+  ISRCTN: string | null;
+  ShortName: string;
+  DateEdited: string | null;
+  UDef6: string | null;
+  Search_Tagged: boolean;
+  TrialRegistrationID: string | null;
+}
+
+export interface ReportDto {
+  CENTRALReportID: number | null;
+  CRGReportID: number;
+  Title: string;
+  Notes: string | null;
+  ReportNumber: number;
+  OriginalTitle: string | null;
+  Authors: string | null;
+  Journal: string | null;
+  Year: number | null;
+  Volume: string | null;
+  Issue: string | null;
+  Pages: string | null;
+  Language: string | null;
+  Abstract: string | null;
+  CENTRALSubmissionStatus: string | null;
+  CopyStatus: string | null;
+  DatetoCENTRAL: string | null;
+  Dateentered: string | null;
+  DateEdited: string | null;
+  Editors: string | null;
+  Publisher: string | null;
+  City: string | null;
+  DupString: string;
+  TypeofReportID: number | null;
+  PublicationTypeID: number;
+  Edition: string | null;
+  Medium: string | null;
+  StudyDesign: string | null;
+  DOI: string | null;
+  UDef3: string | null;
+  ISBN: string | null;
+  UDef5: string | null;
+  PMID: string | null;
+  TrialRegistrationID: string | null;
+  UDef9: string | null;
+  UDef10: string | null;
+  UDef8: string | null;
+  PDFLinks: string | null; 
+}
+
+export interface InterventionDto {
+  ID: number;
+  Description: string;
+}
+
+export interface ConditionDto {
+  ID: number;
+  Description: string;
+}
+
+export interface OutcomeDto {
+  ID: number;
+  Description: string;
+}
+
+export type GetPersonsResponseDto = Record<string, string[]>;
