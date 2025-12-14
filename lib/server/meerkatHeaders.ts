abd6ac65dae826eeaa5c2d8895a1daf0233c0087
Maximilian Mittelhammer <maximittel@outlook.de>
2025-12-14 13:29:10 +0100

    feat: send jwt token to api

--- lib/server/meerkatHeaders.ts
+++ lib/server/meerkatHeaders.ts
@@ -6,16 +6,9 @@ export async function getMeerkatHeaders(accept: string = "application/json") {
   const token = await auth.api.getToken({
     headers: requestHeaders,
   });
-  const apiKey = process.env.MEERKAT_API_KEY;
-
-  if (!apiKey) {
-    throw new Error("MEERKAT_API_KEY is not configured on the server.");
-  }
 
   return {
-    "X-API-Key": apiKey,
-    // TODO: Uncomment this when the token is working
-    // Authorization: `Bearer ${token.token}`,
+    Authorization: `Bearer ${token.token}`,
     Accept: accept,
   } as const;
 }
